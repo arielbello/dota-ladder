3e00e54b7fea55f5fab8ae8276c7ed1e980520c9
ariel <arielfbello@gmail.com>
2020-12-30 19:51:59 -0300

    patch to fix github pages strange url system

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function readTextFile(file, callback) {
 }
 
 //usage:
-readTextFile("/generated/country_stats.csv", function(text){
+readTextFile("/dota-ladder/generated/country_stats.csv", function(text){
     var lines = text.split("\n");
     var table = {};
     for (let i=1; i < lines.length; i++){
