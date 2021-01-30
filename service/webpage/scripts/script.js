cca9ce40a84fc93248b585e93fb613b3a66b0f4d
ariel <arielfbello@gmail.com>
2021-01-29 21:30:48 -0300

    Added datalabels, percentage on hover. Improved layout

--- service/webpage/scripts/script.js
+++ service/webpage/scripts/script.js
@@ -2,13 +2,18 @@ var ctx = document.getElementById('country_chart').getContext('2d');
 var chart;
 let navItems = document.getElementsByClassName("nav-item");
 let navList = Array.from(navItems);
+const BAR_THICKNESS = 35;
+const NO_COUNTRY_INFO_TEXT = "No country information";
+const ON_MOBILE = navigator.userAgent.search("Mobi") !== -1
 const NAV_ITEM_AMERICAS = "nav-item-americas";
 const NAV_ITEM_EUROPE = "nav-item-europe";
 const NAV_ITEM_SEASIA = "nav-item-seasia";
 const NAV_ITEM_CHINA = "nav-item-china";
 const NAV_ITEM_GLOBAL = "nav-item-global";
 
+
 function main(){
+    registerTooltipCustomPositioning();
     let navItemAmericas = document.getElementById(NAV_ITEM_AMERICAS);
     let navItemEurope = document.getElementById(NAV_ITEM_EUROPE);
     let navItemSeasia = document.getElementById(NAV_ITEM_SEASIA);
@@ -23,6 +28,12 @@ function main(){
     //dataset_americas and timestamp is imported from dataset_americas.js
     setNavItemSelected(navItemAmericas);
     updateDataset(dataset_americas, dataset_americas_timestamp);
+}
+
+function registerTooltipCustomPositioning() {
+    Chart.Tooltip.positioners.custom = function(elements, eventPosition) {
+    return { x: eventPosition.x, y: eventPosition.y };
+};
 }
 
 function setNavItemSelected(target) {
@@ -85,70 +96,71 @@ function loadTimestamp(timestamp){
     el.hidden = false;
     //Date in Javascript is in miliseconds since 1970, while python use seconds
     let date = new Date(timestamp * 1000);
-    el.innerHTML = "Last updated: " + date.toLocaleTimeString() + ", " + date.toLocaleDateString();
+    el.innerHTML = "Last updated " + date.toLocaleTimeString() + ", " + date.toLocaleDateString();
 }
 
-function processLabels(labels) {
-//            if (data.groups.country == "country_not_found") {
-//                data.groups.country = "No country info";
-    var out = [];
+function buildChartData(dataDict) {
+    var labels = [];
+    var percentages = [];
+    var total = Object.values( (acc, val) => acc + val);
     var newLabel = "";
-    labels.forEach(label=>{
-        if (label == "country_not_found") {
-            newLabel = "No country information";
+    for (var key in dataDict) {
+        total += dataDict[key];
+        if (key == "country_not_found") {
+            total -= dataDict[key];
+            newLabel = NO_COUNTRY_INFO_TEXT;
         }
-        else if (label.length >= 27) {
-            newLabel = label.slice(0, 27).padEnd(30, ".");
+        else if (key.length >= 27) {
+            newLabel = key.slice(0, 27).padEnd(30, ".");
         }
         else {
-            newLabel = label;
+            newLabel = key;
         }
+        labels.push(newLabel);
+    }
+    for (var key in dataDict) {
+        let percentage = ((dataDict[key] / total) * 100).toFixed(2) + "%"
+        percentages.push(percentage);
+    }
 
-        out.push(newLabel);
-    });
-    return out;
+    return { "labels": labels, "percentages": percentages, "values": Object.values(dataDict) };
 }
 
 //Configure and load the dataset into the table
 function loadTable(data) {
+    //Destroy the previous chart to update
     if (chart) {
-//        chart.data.labels = [];
-//        chart.data.datasets = [];
         chart.destroy();
     }
 
     let numCountries = Object.keys(data).length;
-    let maxNum = Object.values(data)[0];
-    if (numCountries > 2 && Object.values(data)[0]/Object.values(data)[1] > 2) {
-        maxNum = Object.values(data)[1] * 2;
-    }
-
     //Resizes the chart to fit the size of the dataset
     //otherwise it arbitrarily chooses it's size
-    var chartHeight = numCountries * 30;
+    var chartHeight = numCountries * BAR_THICKNESS;
     chartHeight = chartHeight < 50 ? 50 : chartHeight;
     let chartDiv = document.getElementById("chart-container");
     chartDiv.style.height = String(chartHeight) + "px";
+    let chartData = buildChartData(data);
+//    let labels = processLabels(Object.keys(data));
 
-    let labels = processLabels(Object.keys(data));
-
+    window.chartData = chartData;
     //Chart creation with A LOT of options
     chart = new Chart(ctx, {
 
     type: "horizontalBar",
-    showTooltips:false,
 
     data: {
-        labels: labels,
+        labels: chartData["labels"],
         datasets: [{
-            label: 'players',
+            label: 'Players',
             backgroundColor: "rgba(180,200,132,0.2)",
             borderColor: "rgba(180,200,132,1)",
             borderWidth: 2,
             hoverBackgroundColor: "rgba(180,200,132,0.4)",
             hoverBorderColor: "rgba(180,200,132,1)",
-            data: Object.values(data),
-            maxBarThickness: 28,
+//            data: Object.values(data),
+            data: chartData["values"],
+            maxBarThickness: BAR_THICKNESS,
             minBarLength: 20,
             barPercentage: 0.9,
             categoryPercentage: 0.9
@@ -158,20 +170,51 @@ function loadTable(data) {
     options: {
         aspectRatio: chartDiv.clientWidth / chartHeight,
         maintainAspectRatio: true,
+
+        tooltips: {
+            enabled: true,
+            position: "custom",
+            titleFontSize: 14,
+            titleFontStyle: "normal",
+            bodyFontSize: 16,
+            bodyFontStyle: "bold",
+            backgroundColor: "rgb(65,45,60)",
+            callbacks: {
+                label: function(tooltipItem, data) {
+                    if (chartData["labels"][tooltipItem.index] == NO_COUNTRY_INFO_TEXT) {
+                        return "Percentage ignores this value";
+                    }
+                    return chartData["percentages"][tooltipItem.index];
+                }
+            }
+        },
+
+        animation: {
+            duration: ON_MOBILE ? 0 : 1000,
+        },
+
         hover: {
             animationDuration: 0,
         },
         legend: {
-            display: true,
+//            display: true,
             position: "right",
-            align: "start"
+            align: "start",
+            labels: {
+                fontSize: 14,
+                fontColor: "#CACACA",
+            },
         },
         plugins: {
             datalabels: {
-                color: "#CACACA",
-//                align: "end",
-//                anchor: "left",
-                clamp: true,
+                color: "#DADADA",
+                anchor: "end",
+                align: "end",
+                visibility: true,
+                font: {
+                    weight: "normal",
+                    size: 15,
+                },
             }
         },
         title: {
@@ -184,58 +227,21 @@ function loadTable(data) {
         scales: {
             yAxes:[{
                 ticks: {
+//                    mirror: true, //To display labels inside the bars
                     // lineHeight: 2.5, doesn't work
-                    fontSize: 14,
-                    fontColor: "#CACACA"
+                    fontSize: 16,
+                    fontColor: "#DADADA"
                 },
 
             }],
             xAxes:[{
                 type : "linear",
-
                 ticks: {
-                    // suggestedMax: maxNum
-
+//                     suggestedMax: maxNum,
                 }
             }]
         }
     }
     });
 }
-
-//load a static text file asynchronously
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
-}
-
-//load country stats into a dictionary
-function onCountryDataLoad(text) {
-    var lines = text.split("\n");
-    var table = {};
-    for (let i=1; i < lines.length; i++){
-        let regex = /(?<country>[\w\W]+?),(?<num>\d+)/;
-        let data = lines[i].match(regex);
-        if (!data || data.length < 3){
-            console.log("error reading country stats");
-        }
-        else {
-            if (data.groups.country == "country_not_found") {
-                data.groups.country = "No country info";
-            }
-            table[data.groups.country] = data.groups.num;
-        }
-    }
-    loadTable(table);
-}
-
-//Not using asynchronous load for now
-//readTextFile("/scrapping/generated/country_stats.csv", onCountryDataLoad);
-
 main();
